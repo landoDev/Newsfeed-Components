c179f00c8ca8fad708b0fd493c533f40dc25a707
Landon Turner <lturner@LTurner-MBP.lan1>
2020-02-12 18:54:14 -0500

    added menu companents and iteration

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -20,16 +20,43 @@ let menuItems = [
   </div>
 
   The function takes an array as its only argument.
-
+*/
+function createMenu(menuArray){
+  const menu = document.createElement('div');
+  const menuList= document.createElement('ul');
+  menu.appendChild(menuList);
+  /*
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
-
+  */
+  menuArray.forEach((item) => {
+    // make an li
+    // add text to li
+    // add that list item to ul
+    let newItem = document.createElement('li')
+    newItem.textContent = item;
+    menuList.appendChild(newItem);
+  })
+  /*
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
+  */
+//  const menuBtn = document.querySelector('.menu-button');
+  /*
   Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+*/
+// menuBtn.addEventListener('click', () => {
 
+// })
+/*
   Step 5: return the menu component.
+  */
+ return menu
+}
 
+console.log(createMenu(menuItems));
+/*
   Step 6: add the menu component to the DOM.
   
 */
+// const header = document.querySelector('header');
+// header.appendChild(createMenu(menuItems));
